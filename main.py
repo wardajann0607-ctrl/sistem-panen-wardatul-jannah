4d43203645a1ea4e7f48614fb2ef9b4f4f6dc668
Wardatul Jannah <wardatul@example.com>
2026-09-18 10:47:10 +0000

    feat: tambahkan fungsi hitung diskon panen

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def hitung_total_panen(berat_kg, harga_per_kg):
     return berat_kg * harga_per_kg
 
+def hitung_diskon(total_harga, persentase_diskon):
+    return total_harga * (persentase_diskon / 100)
+
 if __name__ == "__main__":
     total = hitung_total_panen(100, 15000)
-    print(f"Total hasil panen: Rp{total}")
-    
+    diskon = hitung_diskon(total, 10)
+    total_akhir = total - diskon
+    print(f"Total awal: Rp{total}")
+    print(f"Diskon: Rp{diskon}")
+    print(f"Total bayar: Rp{total_akhir}")
