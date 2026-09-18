af97b9a140a09ddda0f9968cd32df16fb693d6e6
Wardatul Jannah <wardatul@example.com>
2026-09-18 10:42:12 +0000

    feat: inisialisasi proyek dan fungsi hitung total panen

---
+++ main.py
@@ -0,0 +1,7 @@
+def hitung_total_panen(berat_kg, harga_per_kg):
+    return berat_kg * harga_per_kg
+
+if __name__ == "__main__":
+    total = hitung_total_panen(100, 15000)
+    print(f"Total hasil panen: Rp{total}")
+    
