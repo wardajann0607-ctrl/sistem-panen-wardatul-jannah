f2aa4f446c30855875439b82186affab88e047c0
Nama Kamu <email_kamu@example.com>
2026-09-18 15:42:23 +0000

    feat(Anggota B): tambahkan fungsi cetak laporan panen

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ def hitung_total_panen(berat_kg, harga_per_kg):
 def hitung_diskon(total_harga, persentase_diskon):
     return total_harga * (persentase_diskon / 100)
 
+# Fitur pengerjaan Anggota B: cetak ringkasan laporan
+def cetak_laporan_panen(berat, harga, diskon_persen):
+    total = hitung_total_panen(berat, harga)
+    diskon = hitung_diskon(total, diskon_persen)
+    total_bayar = total - diskon
+    print("=== LAPORAN RINGKASAN PANEN ===")
+    print(f"Total Panen : Rp{total}")
+    print(f"Potongan    : Rp{diskon}")
+    print(f"Total Bayar : Rp{total_bayar}")
+
 if __name__ == "__main__":
-    total = hitung_total_panen(100, 15000)
-    diskon = hitung_diskon(total, 10)
-    total_akhir = total - diskon
-    print(f"Total awal: Rp{total}")
-    print(f"Diskon: Rp{diskon}")
-    print(f"Total bayar: Rp{total_akhir}")
+    cetak_laporan_panen(100, 15000, 10)
+    
