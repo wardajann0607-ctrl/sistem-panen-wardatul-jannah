1349d7ccedc1f753bae6ef0b897ea9bbd7bf0d70
Wardah <wardajann0607@gmail.com>
2026-09-19 11:01:39 +0000

    fix: selesaikan merge conflict pada main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ def hitung_total_panen(berat_kg, harga_per_kg):
 
 if __name__ == "__main__":
     total = hitung_total_panen(100, 15000)
-    print(f"Total Panen Versi Nabila: Rp{total}")
+    # Hasil penyelesaian merge conflict
+    print(f"Total Panen Versi Wardah & Nabila: Rp{total}")
