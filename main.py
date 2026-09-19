1398278fab2962a35dafca7edf1f69e2ad9b3be9
Wardah <wardajann0607@gmail.com>
2026-09-19 16:25:16 +0000

    feat: ubah pesan cetak oleh Wardah

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ def hitung_total_panen(berat_kg, harga_per_kg):
 
 if __name__ == "__main__":
     total = hitung_total_panen(100, 15000)
-    # Hasil penyelesaian merge conflict
-    print(f"Total Panen Versi Wardah & Nabila: Rp{total}")
+    print(f"Total Panen Versi Wardah: Rp{total}")
