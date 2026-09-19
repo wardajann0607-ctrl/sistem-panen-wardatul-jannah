1826816ff6ba26d94b640b15f1dc3dd6ed52b8ca
NamaGitHubmu <email_githubmu@gmail.com>
2026-09-19 10:45:43 +0000

    feat: ubah pesan cetak oleh Anggota B secara independen

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# Modul yang dikerjakan oleh Anggota A (Wardatul Jannah)
 def hitung_total_panen(berat_kg, harga_per_kg):
     return berat_kg * harga_per_kg
 
 if __name__ == "__main__":
     total = hitung_total_panen(100, 15000)
-    print(f"Total Hasil Panen: Rp{total}")
+    print(f"Total Panen Versi Anggota B: Rp{total}")
